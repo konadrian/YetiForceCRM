82c113763728ca173773f1976979c68e4a1bc58b
Adrian Koń <konadrian@wp.pl>
2021-03-08 10:08:30 +0100

    Remove polish letter

--- modules/Calendar/models/QuickCreateRecordStructure.php
+++ modules/Calendar/models/QuickCreateRecordStructure.php
@@ -6,7 +6,7 @@
  *
  * @copyright YetiForce Sp. z o.o
  * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
- * @author    Adrian Koń <m.krzaczkowski@yetiforce.com>
+ * @author    Adrian Kon <m.krzaczkowski@yetiforce.com>
  */
 
 /**
